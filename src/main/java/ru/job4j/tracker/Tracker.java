a3830560d772e830cddd46b86c11b762150c5b10
Sergei Bespalov <sergey.bespalov@protonmail.com>
2023-09-28 21:01:32 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -7,6 +7,17 @@ public class Tracker {
     private int ids = 1;
     private int size = 0;
 
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getId() == id) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
     public Item add(Item item) {
         item.setId(ids++);
         items[size++] = item;
@@ -14,15 +25,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int idx = indexOf(id);
+        return idx >= 0 ? items[idx] : null;
     }
 
     public Item[] findAll() {
@@ -47,5 +51,14 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(rsl, j);
+    }
+
+    public boolean replace(int id, Item item) {
+        int idx = indexOf(id);
+        if (idx >= 0) {
+            items[idx].setName(item.getName());
+            return true;
+        }
+        return false;
     }
 }
