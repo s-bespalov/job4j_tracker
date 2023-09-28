35d3fc8864155313e3ae8a71ef334d0a15bbb1e8
Sergei Bespalov <sergey.bespalov@protonmail.com>
2023-09-28 19:17:01 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[this.size];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                rsl[j] = items[i];
+                j++;
+            }
+        }
+        return Arrays.copyOf(rsl, j);
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl = new Item[size];
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            if (name.equals(items[i].getName())) {
+                rsl[j] = items[i];
+                j++;
+            }
+        }
+        return Arrays.copyOf(rsl, j);
     }
 }
