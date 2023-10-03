a83de85cafa4b144ca4f406faa5c1374dfed366d
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-10-04 00:28:57 +0300

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman frs = new Freshman();
+        Student std = frs;
+        Object obj = frs;
+    }
+}
