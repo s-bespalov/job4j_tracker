ea20bbe6c6e3e8d1936578453fd9b3c8cbb2b0e7
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-10-07 02:23:00 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < values.length; i++) {
+            if (key.equals(values[i])) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element " + key + "not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"Sergei", "Theodore", "Viktoria", "Elena"};
+        try {
+            int idx = indexOf(arr, "Elena");
+            System.out.println("Index of \"Elena\": " + idx);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
