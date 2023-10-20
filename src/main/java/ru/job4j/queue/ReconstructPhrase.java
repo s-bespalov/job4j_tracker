4e1921cf24b3f3f04c11fa3ab1e944a6fe306161
Sergei Bespalov <sergey.bespalov@protonmail.com>
2023-10-20 13:52:51 +0300

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,41 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+import java.util.Iterator;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder rsl = new StringBuilder();
+        Iterator<Character> iterator = evenElements.iterator();
+        for (int i = 0; iterator.hasNext(); i++) {
+            Character ch = iterator.next();
+            if (i % 2 == 0) {
+                rsl.append(ch);
+            }
+        }
+        return rsl.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder rsl = new StringBuilder();
+        Iterator<Character> iterator = descendingElements.descendingIterator();
+        while (iterator.hasNext()) {
+            rsl.append(iterator.next());
+        }
+        return rsl.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
