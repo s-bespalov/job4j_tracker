77ee507d1ebeb4f42c0c904d6e3a9bb74660f15a
Sergei Bespalov <sergey.bespalov@protonmail.com>
2023-10-20 12:51:18 +0300

    1. Интерфейс Queue [#504942 #453755]

--- src/main/java/ru/job4j/queue/AppleStore.java
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -5,26 +5,25 @@ import java.util.Queue;
 public class AppleStore {
     private final Queue<Customer> queue;
     private final int count;
-    private Customer lastHappy;
 
     public AppleStore(Queue<Customer> queue, int count) {
         this.queue = queue;
         this.count = count;
-        process();
-    }
-
-    private void process() {
-        for (int i = 0; i < count; i++) {
-            lastHappy = queue.poll();
-        }
     }
 
     public String getLastHappyCustomer() {
-        return lastHappy.name();
+        Customer lastHappy = null;
+        for (int i = 0; i < count; i++) {
+            lastHappy = queue.poll();
+        }
+        return lastHappy != null ? lastHappy.name() : null;
     }
 
     public String getFirstUpsetCustomer() {
-        Customer client = queue.peek();
-        return client != null ? client.name() : null;
+        for (int i = 0; i < count; i++) {
+            queue.poll();
+        }
+        Customer firstUpset = queue.peek();
+        return firstUpset != null ? firstUpset.name() : null;
     }
 }
