cad51fbf3aaa11cbbcff85f7af9f7dc0de2ee707
Sergei Bespalov <sergey.bespalov@protonmail.com>
2023-10-20 15:32:00 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("theodor1378@tmpmail.com", "Theodor");
+        map.put("nina8756@tmpmail.com", "Nina");
+        for (String key : map.keySet()) {
+            System.out.println(map.get(key));
+        }
+    }
+}
