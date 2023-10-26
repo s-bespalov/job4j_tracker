320cd4fc0a4e793d31b00635fdc04d3974a6866a
Sergei Bespalov <sergey.bespalov@protonmail.com>
2023-10-26 22:55:04 +0300

    2.1. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,20 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a",  "ccc", "dddd", "bb");
+        Comparator<String> comparator = (left, right) -> {
+            int rsl = Integer.compare(right.length(), left.length());
+            System.out.printf("compare - %d : %d result: %d\n", right.length(), left.length(), rsl);
+            return rsl;
+        };
+        strings.sort(comparator);
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+}
