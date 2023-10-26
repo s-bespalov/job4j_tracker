d7da07764a4076fbe73d1b844a7cfaae8d906a77
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-10-26 10:36:36 +0300

    3. Отсортировать департаменты [#34352 #455355]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,15 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String[] arrLeft = left.split("/");
         String[] arrRight = right.split("/");
         int first = arrRight[0].compareTo(arrLeft[0]);
-        if (first != 0) {
-            return first;
-        }
-        for (int i = 1; i < Math.min(arrLeft.length, arrRight.length); i++) {
-            int second = arrLeft[i].compareTo(arrRight[i]);
-            if (second != 0) {
-                return second;
-            }
-        }
-        return Integer.compare(arrLeft.length, arrRight.length);
+        return first != 0 ? first : left.compareTo(right);
     }
 }
