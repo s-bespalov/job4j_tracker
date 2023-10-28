c7dd748074ae1db8bdcb2c9062d46a61ab630a2b
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-10-29 00:51:20 +0300

    4. Перенос методов в Stream API [#279131]

---
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -0,0 +1,12 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+public class ProductLabel {
+    public List<String> generateLabels(List<Product> products) {
+        return products.stream()
+                .filter(p -> p.getStandard() - p.getActual() <= 3)
+                .map(p -> new Label(p.getName(), p.getPrice() / 2).toString())
+                .toList();
+    }
+}
