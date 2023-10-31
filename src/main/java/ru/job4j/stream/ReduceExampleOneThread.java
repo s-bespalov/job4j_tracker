4113d591f34cae65be154b7a026f56b05bbf2447
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-11-01 01:30:07 +0300

    15. Агрегатный метод reduce() [#504929 #456881]

---
+++ src/main/java/ru/job4j/stream/ReduceExampleOneThread.java
@@ -0,0 +1,36 @@
+package ru.job4j.stream;
+
+import ru.job4j.pojo.Person;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReduceExampleOneThread {
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        int sum = people.stream()
+                .reduce(
+                        0,
+                        (a, b) -> {
+                            int rsl = a;
+                            if (b.getAge() > 25) {
+                                rsl += b.getAge();
+                            }
+                            System.out.println("reduce accumulator, a: " + a + " b:" + b + "result: " + rsl);
+                            return rsl;
+                        },
+                        (a, b) -> {
+                            int rsl = a + b;
+                            System.out.println("reduce combiner, a: " + a + " b:" + b + " result: " + rsl);
+                            return 0;
+                        }
+                );
+        System.out.println(sum);
+    }
+}
