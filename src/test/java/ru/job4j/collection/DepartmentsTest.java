cade4551a2dd015ed920f23d06667807156f142b
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-11-06 16:15:26 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -11,36 +11,36 @@ public class DepartmentsTest {
 
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1", "k2/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> input = List.of("k1/sk1", "k2/sk1");
+        List<String> expect = List.of("k1", "k1/sk1", "k2", "k2/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
 
     @Test
     public void whenMissed2() {
-        List<String> input = Arrays.asList("K1/SK1/SSK2", "K2/SK1/SSK2");
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK2", "K2", "K2/SK1", "K2/SK1/SSK2");
+        List<String> input = List.of("K1/SK1/SSK2", "K2/SK1/SSK2");
+        List<String> expect = List.of("K1", "K1/SK1", "K1/SK1/SSK2", "K2", "K2/SK1", "K2/SK1/SSK2");
         List<String> result = Departments.fillGaps(input);
         assertThat(result).containsSequence(expect);
     }
 
     @Test
     public void whenMissedSomeDepartmentsCode() {
-        List<String> input = Arrays.asList(
+        List<String> input = List.of(
                 "K1/SK1/SSK1",
                 "K1/SK1/SSK2",
                 "K2/SK1/SSK1"
         );
-        List<String> expect = Arrays.asList(
+        List<String> expect = List.of(
                 "K1",
                 "K1/SK1",
                 "K1/SK1/SSK1",
