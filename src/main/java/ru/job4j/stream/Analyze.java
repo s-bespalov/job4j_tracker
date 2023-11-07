2d548b95a7275d521082f01256cd2e2bcca92c49
Sergey Bespalov <sergey.bespalov@protonmail.com>
2023-11-08 02:23:26 +0300

    3. Аттестация [#49777 #458282]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -57,7 +57,6 @@ public class Analyze {
                 .flatMap(p -> p.subjects().stream())
                 .collect(Collectors.groupingBy(
                         Subject::name,
-                        LinkedHashMap::new,
                         Collectors.summingDouble(Subject::score)))
                 .entrySet()
                 .stream()
